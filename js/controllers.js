a2b68fdf0081bd9c75126131a44b4e4cb4848644
HamishHD <dsouza.hamish@gmail.com>
2014-10-08 20:34:10 +0530

    Fixtures

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,17 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
-.controller('FixturesCtrl', function($scope) {
-  $scope.friends = Friends.all();
+.controller('FixturesCtrl', function($scope, MyServices ) {
+    
+    var fuxturesuccess = function(data, status)
+    {
+        $scope.fixturedata = data;
+        console.log($scope.fixturedata);
+    };
+    
+    console.log("fixtures function");
+    MyServices.getfixtures().success(fuxturesuccess);
+
 })
 
 .controller('PixoloCtrl', function($scope, $stateParams) {
